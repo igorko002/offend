fbf4fca3fef300d37fbbdc622726f7dbdab8de76
Igor <gudima.dog@gmail.com>
2023-04-03 20:12:41 +0300

    Add project files.

---
+++ Project9/main.c
@@ -0,0 +1,268 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t\t");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("         ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t      ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("     ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t        ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s(" ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t          ");
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t          ");
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t          ");
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t        ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s(" ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 6; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t      ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("     ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 6; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("  ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t    ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("         ");
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\n");
+	printf_s("\t");
+	for (int i = 0; i < 6; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t\t");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+	printf_s("\t       ");
+	for (int i = 0; i < 4; i++)
+	{
+		printf_s(" * ");
+	}
+
+	return 0;
+}
